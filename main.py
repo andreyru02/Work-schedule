61a60c07c668b62c33eec242f4c5920a4805189c
Andrey Chebunin <andrey.tchebunin@yandex.ru>
2021-01-22 18:41:59 +0800

    Add date_time file
    Add date_time file
    
    Added update date button
    
    Added update date button
    
    Added update date button
    
    Added database
    
    Added functions administrator

--- main.py
+++ main.py
@@ -3,29 +3,64 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.types import InputFile
+import glob
+from sql import SQL
 
 bot = Bot(token="TOKEN")
 dp = Dispatcher(bot)
+db = SQL('bd.db')
 
-keyboard = InlineKeyboardMarkup()
+keyboard = InlineKeyboardMarkup(row_width=2)
 graph = InlineKeyboardButton(text='График', callback_data="graph")
 list = InlineKeyboardButton(text='Лист', callback_data="list")
-keyboard.add(graph, list)
+date_file = InlineKeyboardButton(text='Дата обновления графика', callback_data="date_file")
+keyboard.add(graph, list, date_file)
+
+adm_keyboard = InlineKeyboardMarkup()
+prn_bd = InlineKeyboardButton(text='Отправить базу', callback_data='send_bd')
+adm_keyboard.add(prn_bd)
 
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await bot.send_message(message.from_user.id, "Выбери действие!", reply_markup=keyboard)
+    if message.from_user.id == 860916279:
+        await bot.send_message(message.from_user.id, 'Выбери действие!', reply_markup=adm_keyboard)
+    else:
+        await bot.send_message(message.from_user.id, "Выбери действие!", reply_markup=keyboard)
+        if not db.subscriber_exists(message.from_user.id):
+            # если пользователя нет в базе - добавляем его
+            db.add_subscriber(message.from_user.id, message.from_user.first_name, message.from_user.username)
 
 
 @dp.callback_query_handler()
 async def menu(call: types.CallbackQuery):
     if call.data == 'graph':
-        await bot.send_message(call.from_user.id, 'Файл загружается.')
-        file_graph = InputFile('/home/sa/work_schedule/data/Grafik.xlsm')
-        await bot.send_document(call.from_user.id, file_graph, reply_markup=keyboard)
+        if not db.subscriber_exists(call.from_user.id):
+            await bot.send_message(call.from_user.id,
+                                   'Для продолжения работы требуется перезапуск бота или отправить команду "/start"')
+        else:
+            await bot.send_message(call.from_user.id, 'Файл загружается.')
+            file_graph = InputFile(f'{glob.glob("/home/sa/work_schedule/data/*.xlsm")[0]}')
+            await bot.send_document(call.from_user.id, file_graph, reply_markup=keyboard)
+            db.add_count_graph(call.from_user.id)
     elif call.data == 'list':
-        await bot.send_message(call.from_user.id, 'Функционал еще не реализован.', reply_markup=keyboard)
+        if not db.subscriber_exists(call.from_user.id):
+            await bot.send_message(call.from_user.id,
+                                   'Для продолжения работы требуется перезапуск бота или отправить команду "/start"')
+        else:
+            await bot.send_message(call.from_user.id, 'Функционал еще не реализован.', reply_markup=keyboard)
+            db.add_count_list(call.from_user.id)
+    elif call.data == 'date_file':
+        if not db.subscriber_exists(call.from_user.id):
+            await bot.send_message(call.from_user.id,
+                                   'Для продолжения работы требуется перезапуск бота или отправить команду "/start"')
+        else:
+            send_date_file = glob.glob("/home/sa/work_schedule/data/*.xlsm")[0][34:-5]
+            await bot.send_message(call.from_user.id, 'Дата изменения графика: ' + send_date_file,
+                                   reply_markup=keyboard)
+            db.add_count_date_file(call.from_user.id)
+    elif call.data == 'send_bd' and call.from_user.id == 860916279:
+        await bot.send_message(call.from_user.id, db.read_bd())
     else:
         await bot.send_message(call.from_user.id, 'Неизвестная команда!', reply_markup=keyboard)
 
---
+++ sql.py
@@ -0,0 +1,44 @@
+import sqlite3
+import pandas as pd
+
+class SQL:
+
+    def __init__(self, database):
+        """Подключение к БД"""
+        self.connection = sqlite3.connect(database)
+        self.cursor = self.connection.cursor()
+
+    def subscriber_exists(self, user_id):
+        """Проверяем, есть ли уже пользователь в базе"""
+        with self.connection:
+            result = self.cursor.execute('SELECT * FROM "subscriptions" WHERE "user_id" = ?', (user_id,)).fetchall()
+            return bool(len(result))
+
+    def add_subscriber(self, user_id, first_name, username):
+        """Добавляем нового пользователя"""
+        with self.connection:
+            return self.cursor.execute("INSERT INTO 'subscriptions' ('user_id', 'first_name', 'username') VALUES(?,?,?)", (user_id, first_name, username))
+
+    def add_count_graph(self, user_id):
+        """Увеличиваем счетчик запросов графика"""
+        with self.connection:
+            return self.cursor.execute("UPDATE 'subscriptions' SET count_graph = count_graph + 1 WHERE user_id = ?", (user_id,))
+
+    def add_count_list(self, user_id):
+        """Увеличиваем счетчик запросов листов"""
+        with self.connection:
+            return self.cursor.execute("UPDATE 'subscriptions' SET count_list = count_list + 1 WHERE user_id = ?", (user_id,))
+
+    def add_count_date_file(self, user_id):
+        """Увеличиваем счетчик запросов даты файла"""
+        with self.connection:
+            return self.cursor.execute("UPDATE 'subscriptions' SET date_file = date_file + 1 WHERE user_id = ?", (user_id,))
+
+    def read_bd(self):
+        """Вывод информации с БД в чат"""
+        with self.connection:
+            return self.cursor.execute('SELECT * FROM "subscriptions"').fetchall()
+
+    def close(self):
+        """Закрываем соединение с БД"""
+        self.connection.close()
